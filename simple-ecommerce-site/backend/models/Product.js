4ec64dbaadd77869447c7ce1f18d94b3e88446c9
Ndumiso Zulu <ndzuljhb025@student.wethinkcode.co.za>
2026-06-06 21:43:09 +0200

    Fix formatting in Product.js

--- simple-ecommerce-site/backend/models/Product.js
+++ simple-ecommerce-site/backend/models/Product.js
@@ -1,4 +1,5 @@
-const mongoose = require('mongoose');const productSchema = new mongoose.Schema({
+const mongoose = require('mongoose');
+const productSchema = new mongoose.Schema({
   name: String,
   price: Number,
   description: String
