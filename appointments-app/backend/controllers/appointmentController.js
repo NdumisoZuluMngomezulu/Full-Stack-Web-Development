09210e4bf089585c642e3e6e8146c6816f187a3b
Ndumiso Zulu <ndzuljhb025@student.wethinkcode.co.za>
2026-06-17 13:56:12 +0200

    "feat : createAppointment in controller"

---
+++ appointments-app/backend/controllers/appointmentController.js
@@ -0,0 +1,51 @@
+const Appointment = require('../models/Appointment');
+const User = require('../models/User');
+
+//GET all appointments for any user, must be logged in
+exports.getAllAppointments = async(req, res) => {
+    try{
+        let query = {};
+        //attatch patientid to query if the user is a patient so only the patients appointment shows up
+        if (req.user.role === 'patient'){
+            query.patientId = req.user.id;
+        }
+
+        const appointments = await Appointment.find(query)
+            .populate('doctorId', 'name-email')
+            .populate('patientId', 'name email')
+            .sort({ date: 1});
+        res.json(appointments);
+    } catch (err){
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+};
+
+//POST to create appointments for any user, must be logged in
+exports.createAppointment = async (req, res) => {
+    const {doctorEmail, date, reason} = req.body;
+    try {
+        const doctor = await User.findOne({
+            email: doctorEmail, role: 'doctor'
+        });
+        if (new Date(date) < new Date()){
+            return res.status(400).json({ msg: 'Cannot book appointment in the past'});
+        }
+
+        const newAppointment = new Appointment({
+            doctorId: doctor._id,
+            patientId: req.user.id,
+            date,
+            reason
+        });
+
+        const appointment = await newAppointment.save();
+        await appointment.populate('doctorId', 'name email');
+        await appointment.populate('patientId', 'name email');
+        res.json(appointment);
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+};
